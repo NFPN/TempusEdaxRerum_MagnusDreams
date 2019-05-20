0b3ce58e15bb23516b938db6799a32b1ef369c8b
Nícolas Sanchez <nico_sansoares@hotmail.com>
2019-05-19 21:31:47 -0300

    Adicionei algumas coisas no bd , arrumei no codigo e criei um WIndow novo

---
+++ MagnusDreams/Views/SelectName.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MagnusDrems.DAO;//para pegar o comando sql
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MagnusDreams.Views
+{
+    /// <summary>
+    /// Lógica interna para SelectName.xaml
+    /// </summary>
+
+    public partial class SelectName : Window
+    {
+        ComandosSQL comandos = new ComandosSQL();
+        public SelectName()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGame_Clicked(object sender, RoutedEventArgs e)
+        {
+
+            //Insere o texto inserido do player no Banco de dados
+            comandos.InsertData(lbTextNamePlayer.Text);
+        }
+    }
+}
