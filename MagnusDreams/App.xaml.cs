b98174ca0cf1b3a731089806a7f69c14fc8f5f83
Nícolas Sanchez <nico_sansoares@hotmail.com>
2019-05-23 17:20:25 -0300

    Insert the hover element on the game.

--- MagnusDreams/App.xaml.cs
+++ MagnusDreams/App.xaml.cs
@@ -13,5 +13,9 @@ namespace MagnusDreams
     /// </summary>
     public partial class App : Application
     {
+        private void Setter_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
     }
 }
